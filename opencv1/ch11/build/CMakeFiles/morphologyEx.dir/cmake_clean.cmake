09110a0b647b4bbffd1186c0ca6c59d2603a70b9
younghanChoi <chldydgks20082@gmail.com>
2024-07-26 16:21:40 +0900

    update

---
+++ opencv1/ch11/build/CMakeFiles/morphologyEx.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/morphologyEx.dir/morphologyEx.cpp.o"
+  "CMakeFiles/morphologyEx.dir/morphologyEx.cpp.o.d"
+  "morphologyEx"
+  "morphologyEx.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/morphologyEx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ opencv1/ch11/build/CMakeFiles/threthold.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/threthold.dir/threthold.cpp.o"
+  "CMakeFiles/threthold.dir/threthold.cpp.o.d"
+  "threthold"
+  "threthold.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/threthold.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
