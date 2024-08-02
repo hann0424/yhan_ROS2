dc88a71d7b7b4e7f1e1aaea84620580b0f39249b
younghanChoi <chldydgks20082@gmail.com>
2024-08-02 11:35:22 +0900

    tt

---
+++ yhan_ws/build/move_t_sim/CMakeFiles/moveTurtleSim2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/moveTurtleSim2.dir/src/moveTurtle2.cpp.o"
+  "CMakeFiles/moveTurtleSim2.dir/src/moveTurtle2.cpp.o.d"
+  "CMakeFiles/moveTurtleSim2.dir/src/moveTurtleSim2.cpp.o"
+  "CMakeFiles/moveTurtleSim2.dir/src/moveTurtleSim2.cpp.o.d"
+  "moveTurtleSim2"
+  "moveTurtleSim2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/moveTurtleSim2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
